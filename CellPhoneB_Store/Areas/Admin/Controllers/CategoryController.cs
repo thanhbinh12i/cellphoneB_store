fc697cb4372dfabe9746ceb0a9ccb8d9d727a241
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-04-04 15:50:43 +0700

    [add] paginate demo

--- CellPhoneB_Store/Areas/Admin/Controllers/CategoryController.cs
+++ CellPhoneB_Store/Areas/Admin/Controllers/CategoryController.cs
@@ -19,10 +19,31 @@ namespace CellPhoneB_Store.Areas.Admin.Controllers
 			_dataContext = dataContext;
 		}
         [Route("Index")]
-        public async Task<IActionResult> Index()
-		{
-			return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync());
-		}
+        public async Task<IActionResult> Index(int pg = 1)
+        {
+            List<CategoryModel> category = _dataContext.Categories.ToList(); //33 datas
+
+
+            const int pageSize = 10; //10 items/trang
+
+            if (pg < 1) //page < 1;
+            {
+                pg = 1; //page ==1
+            }
+            int recsCount = category.Count(); //33 items;
+
+            var pager = new Paginate(recsCount, pg, pageSize);
+
+            int recSkip = (pg - 1) * pageSize; //(3 - 1) * 10; 
+
+            //category.Skip(20).Take(10).ToList()
+
+            var data = category.Skip(recSkip).Take(pager.PageSize).ToList();
+
+            ViewBag.Pager = pager;
+
+            return View(data);
+        }
         [Route("Create")]
         public async Task<IActionResult> Create()
         {
