5efcdc8f3270a59e9388c3e2368dc3fdd971a6c8
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-04-14 15:27:00 +0700

    [add] call api tỉnh thành phố Việt Nam

---
+++ CellPhoneB_Store/Areas/Admin/Controllers/ShippingController.cs
@@ -0,0 +1,22 @@
+﻿using CellPhoneB_Store.Respository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CellPhoneB_Store.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/Shipping")]
+	[Authorize]
+	public class ShippingController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public ShippingController(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- CellPhoneB_Store/Respository/DataContext.cs
+++ CellPhoneB_Store/Respository/DataContext.cs
@@ -17,11 +17,10 @@ namespace CellPhoneB_Store.Respository
 		public DbSet<OrderDetails> OrderDetails { get; set; }
 		public DbSet<RatingModel> Ratings { get; set; }
 		public DbSet<SliderModel> Sliders { get; set; }
-
         public DbSet<ContactModel> Contact { get; set; }
 		public DbSet<CompareModel> Compares { get; set; }
 		public DbSet<WishlistModel> Wishlist { get; set; }
         public DbSet<ProductQuantityModel> ProductQuantity { get; set; }
-
+		public DbSet<ShippingModel> Shippings { get; set; }
     }
 }
