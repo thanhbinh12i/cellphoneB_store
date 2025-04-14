eb206f467ff6b2df077cc423b95aa1c3e9581f0e
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-04-14 15:54:03 +0700

    [add] shipping cost management

--- CellPhoneB_Store/Areas/Admin/Controllers/ShippingController.cs
+++ CellPhoneB_Store/Areas/Admin/Controllers/ShippingController.cs
@@ -1,6 +1,8 @@
-﻿using CellPhoneB_Store.Respository;
+﻿using CellPhoneB_Store.Models;
+using CellPhoneB_Store.Respository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CellPhoneB_Store.Areas.Admin.Controllers
 {
@@ -14,9 +16,45 @@ namespace CellPhoneB_Store.Areas.Admin.Controllers
 		{
 			_dataContext = context;
 		}
-		public IActionResult Index()
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var shippingList = await _dataContext.Shippings.ToListAsync();
+            ViewBag.Shippings = shippingList;
+            return View();
+        }
+		[HttpPost]
+		[Route("StoreShipping")]
+		public async Task<IActionResult> StoreShipping(ShippingModel shippingModel, string ward, string district, string city, decimal price)
 		{
-			return View();
-		}
-	}
+            shippingModel.City = city;
+            shippingModel.District = district;
+            shippingModel.Ward = ward;
+            shippingModel.Price = price;
+			try
+			{
+				var existingShipping = await _dataContext.Shippings.AnyAsync(x => x.City == city && x.District == district && x.Ward == ward);
+				if (existingShipping)
+				{
+					return Ok(new { duplicate = true, message = "Dữ liệu trùng lặp" });
+				}
+				_dataContext.Shippings.Add(shippingModel);
+				await _dataContext.SaveChangesAsync();
+                return Ok(new { success = true, message = "Thêm shipping thành công" });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while adding shipping.");
+            }
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            ShippingModel shipping = await _dataContext.Shippings.FindAsync(Id);
+
+            _dataContext.Shippings.Remove(shipping);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Shipping đã được xóa thành công";
+            return RedirectToAction("Index");
+        }
+    }
 }
