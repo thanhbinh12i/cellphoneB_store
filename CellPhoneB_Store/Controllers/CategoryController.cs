5c4d84baddfb47a6a8f7524da7cb516ee278c45c
thanhbinh12ii <nptbinh17092004@gmail.com>
2025-04-17 14:59:30 +0700

    [add] filter product

--- CellPhoneB_Store/Controllers/CategoryController.cs
+++ CellPhoneB_Store/Controllers/CategoryController.cs
@@ -12,16 +12,64 @@ namespace CellPhoneB_Store.Controllers
 		{
 			_dataContext = context;
 		}
-		public async Task<IActionResult> Index(string Slug="")
+		public async Task<IActionResult> Index(string Slug="", string sort_by = "", string startprice = "", string endprice = "")
 		{
 			CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
 			if(category == null)
 			{
 				return RedirectToAction("Index");
 			}
-			var productsByCategory = _dataContext.Products.Where(c => c.CategoryId == category.Id);
+			IQueryable<ProductModel> productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+			var count = await productsByCategory.CountAsync();
+			if(count > 0)
+			{
+				if (sort_by == "price_increase")
+				{
+					productsByCategory = productsByCategory.OrderBy(p => p.Price);
+				}
+				else if (sort_by == "price_decrease")
+				{
+					productsByCategory = productsByCategory.OrderByDescending(p => p.Price);
+				}
+				else if (sort_by == "price_newest")
+				{
+					productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+				}
+				else if (sort_by == "price_oldest")
+				{
+					productsByCategory = productsByCategory.OrderBy(p => p.Id);
+				}
+				else if (startprice != "" && endprice != "")
+				{
+					decimal startPriceValue;
+					decimal endPriceValue;
 
-			return View(await productsByCategory.OrderByDescending(c => c.Id).ToListAsync());
+					if (decimal.TryParse(startprice, out startPriceValue) && decimal.TryParse(endprice, out endPriceValue))
+					{
+						productsByCategory = productsByCategory.Where(p => p.Price >= startPriceValue && p.Price <= endPriceValue);
+					}
+					else
+					{
+						productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+					}
+				}
+				else
+				{
+					productsByCategory = productsByCategory.OrderByDescending(p => p.Id);
+				}
+
+				decimal minPrice = await productsByCategory.MinAsync(p => p.Price);
+				decimal maxPrice = await productsByCategory.MaxAsync(p => p.Price);
+
+
+				ViewBag.sort_key = sort_by;
+
+				ViewBag.count = count;
+
+				ViewBag.minprice = minPrice;
+				ViewBag.maxprice = maxPrice;
+			}
+			return View(await productsByCategory.ToListAsync());
 		}
 	}
 }
